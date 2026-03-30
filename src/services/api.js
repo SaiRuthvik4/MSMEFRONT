e09bb106d77dc0bb5db3b07fe4429414a59b99aa
SaiRuthvik4 <168407840+SaiRuthvik4@users.noreply.github.com>
2026-03-30 23:22:06 +0530

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://host.docker.internal:8080', // backend URL
+    baseURL: 'https://msme-backend.onrender.com/api/applicants', // backend URL
 });
 
 export default api;
